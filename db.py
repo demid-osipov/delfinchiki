9d8aea977f93c6b5cb1682a0dde4df067a7f9c60
Demid Osipov <demid2001@gmail.com>
2020-07-06 23:30:33 +0300

    программа для sql-инъекции

---
+++ db.py
@@ -0,0 +1,16 @@
+import MySQLdb
+id = input("Enter colour id...")
+sql = 'SELECT colour FROM colours WHERE colour_id= '+ id +''
+print(sql)
+
+conn = MySQLdb.connect('localhost', 'root', '11012001', 'colours')
+cursor = conn.cursor()
+ 
+cursor.execute(sql)
+ 
+# Получаем данные.
+row = cursor.fetchone()
+print(row)
+ 
+# Разрываем подключение.
+conn.close()
